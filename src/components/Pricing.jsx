dce03f5c0d4a5aac3997acb9c1558dded919969f
hcncc <sbombastic@gmail.com>
2024-03-23 12:34:19 +0100

    feat: Title modified in index page

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,7 @@
+export function Pricing(){
+    return (
+        <div className="mt-20">
+            <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wide">Pricing</h2>
+        </div>
+    )
+}
